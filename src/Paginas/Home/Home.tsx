69c400b54a7a81fded190c668e7c1b666d5b676e
Fernanda <fernandalanza@outlook.com>
2022-07-27 17:29:16 -0300

    Adicionando arquivos

---
+++ src/Paginas/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+        <h1 className='titulo'>Home</h1>
+        <img src="https://static.escolakids.uol.com.br/conteudo_legenda/a7d81be3467084957ac91636dcd8ae68.jpg" alt="Mar" className='img' />
+        </>
+    );
+
+}
+
+export default Home;
