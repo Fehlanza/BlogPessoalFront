e83cea39568bbbd01e4dd1f94f102e1d1d5fad75
Fernanda <fernandalanza@outlook.com>
2022-08-05 20:25:18 -0300

    adicionando paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,58 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
-import Home from './Paginas/Home/Home';
-import Login from './Paginas/login/Login';
-import CadastroUsuario from './Paginas/cadastroUsuario/CadatroUsuario';
+
 import './App.css';
 import ListaTema from './components/temas/listatema/ListaTema';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import Home from './Paginas/Home/Home';
+import CadastroUsuario from './Paginas/cadastroUsuario/CadatroUsuario';
 import ListaPostagem from './components/temas/postagens/listapostagem/ListaPostagem';
+import CadastroPost from './components/temas/postagens/cadastroPost/CadastroPost';
+import DeletarPostagem from './components/temas/postagens/deletarPostagem/DeletarPostagem';
+import Login from './Paginas/login/Login';
+
 
 function App() {
   return (
     <Router>
       <Navbar />
       <div style={{ minHeight: '100vh' }}>
-        <Routes> // Antigo Switch
+        <Routes>
+
           <Route path="/" element={<Login />} />
+
           <Route path="/login" element={<Login />} />
+
           <Route path="/home" element={<Home />} />
-          <Route path="/cadastrousuario" element={<CadastroUsuario />}/>
-          <Route path="/temas" element={<ListaTema />}/>
-          <Route path="/posts" element={<ListaPostagem />}/>
+
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+
+          <Route path="/temas" element={<ListaTema />} />
+
+          <Route path="/posts" element={<ListaPostagem />} />
+
+          <Route path="/formularioPostagem" element={<CadastroPost />} />
+
+          <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+
+          <Route path="/formularioTema" element={<CadastroTema />} />
+
+          <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
+
+
         </Routes>
       </div>
       <Footer />
+
     </Router>
-  )
+  );
 }
+
 export default App;
--- src/Paginas/cadastroUsuario/CadatroUsuario.tsx
+++ src/Paginas/cadastroUsuario/CadatroUsuario.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, ChangeEvent} from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import { Box } from '@mui/material';
 import { Button, Grid, TextField, Typography } from '@material-ui/core';
 import { Link, useNavigate } from 'react-router-dom';
@@ -7,11 +7,11 @@ import User from '../../models/User';
 import './CadastroUsuario.css';
 
 function CadastroUsuario() {
-    
+
     let navigate = useNavigate();
 
-    const [confirmarSenha,setConfirmarSenha] = useState<String>("")
-    
+    const [confirmarSenha, setConfirmarSenha] = useState<String>("")
+
     const [user, setUser] = useState<User>(
         {
             id: 0,
@@ -35,7 +35,7 @@ function CadastroUsuario() {
     }, [userResult])
 
 
-    function confirmarSenhaHandle(e: ChangeEvent<HTMLInputElement>){
+    function confirmarSenhaHandle(e: ChangeEvent<HTMLInputElement>) {
         setConfirmarSenha(e.target.value)
     }
 
@@ -50,11 +50,11 @@ function CadastroUsuario() {
     }
     async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
         e.preventDefault()
-        if(confirmarSenha == user.senha){
-        cadastroUsuario(`/usuarios/cadastrar`, user, setUserResult)
-       alert('Usuario cadastrado com sucesso.')
-        }else{
-           alert('Dados inconsistentes. Favor verificar as informações de cadastro.')
+        if (confirmarSenha == user.senha) {
+            await cadastroUsuario('/usuarios/cadastrar', user, setUserResult)
+            alert('Usuario cadastrado com sucesso.')
+        } else {
+            alert('Dados inconsistentes. Favor verificar as informações de cadastro.')
         }
     }
 
@@ -63,21 +63,21 @@ function CadastroUsuario() {
             <Grid item xs={6} className='imagem2'></Grid>
             <Grid item xs={6} alignItems='center'>
                 <Box paddingX={10}>
-                    <form onSubmit ={onSubmit}>
+                    <form onSubmit={onSubmit}>
                         <Typography variant='h3' gutterBottom color='textPrimary' align='center' className="textos2"> Cadastrar</Typography>
                         <TextField value={user.nome} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='nome' label='Nome' variant='outlined' name='nome' margin='normal' fullWidth></TextField>
-                        <TextField  value={user.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='Usuario' variant='outlined' name='usuario' margin='normal' fullWidth></TextField>
+                        <TextField value={user.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='Usuario' variant='outlined' name='usuario' margin='normal' fullWidth></TextField>
                         <TextField value={user.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='Senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth></TextField>
-                        <TextField value={confirmarSenha} onChange={(e: ChangeEvent<HTMLInputElement>)=> confirmarSenhaHandle(e)} id='confirmarSenha' label='confirmarSenha' variant='outlined' name='confirmarSenha' margin='normal' type='password' fullWidth></TextField>
+                        <TextField value={confirmarSenha} onChange={(e: ChangeEvent<HTMLInputElement>) => confirmarSenhaHandle(e)} id='confirmarSenha' label='confirmarSenha' variant='outlined' name='confirmarSenha' margin='normal' type='password' fullWidth></TextField>
                         <Box marginTop={2} textAlign='center'>
                             <Link to='/login' className='text-decorator-none2'>
                                 <Button variant='contained' color='secondary' className='btnCancelar'>
                                     Cancelar
                                 </Button>
                             </Link>
-                            <Button type= 'submit' variant='contained' color='primary'>
-                                    Cadastrar
-                                </Button>
+                            <Button type='submit' variant='contained' color='primary'>
+                                Cadastrar
+                            </Button>
                         </Box>
                     </form>
                 </Box>
