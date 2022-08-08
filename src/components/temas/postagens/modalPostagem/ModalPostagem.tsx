68c826d2ac3e95c4555e6cbb06ee49ce53d555a6
Fernanda <fernandalanza@outlook.com>
2022-08-08 10:18:13 -0300

    adicionando correções

--- src/components/temas/postagens/modalPostagem/ModalPostagem.tsx
+++ src/components/temas/postagens/modalPostagem/ModalPostagem.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import {Button} from "@material-ui/core"
-import Modal from '@material-ui/core/Modal';
-import { Box } from '@mui/material';
+import { Box, Modal } from '@mui/material';
 import CloseIcon from '@material-ui/icons/Close';
 import './ModalPostagem.css';
 import CadastroPost from '../cadastroPost/CadastroPost';
